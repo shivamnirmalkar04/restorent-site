a3ca4003ad77aa7a55324a65daf98c6c226e5255
SHIVAM NIRMALKAR <106927883+shivamnirmalkar04@users.noreply.github.com>
2024-04-21 13:34:12 +0530

    Add files via upload

---
+++ p20(wd).html/script.js
@@ -0,0 +1,64 @@
+let menu=document.querySelector('#menu-bars');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let section= document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header .navbar a');
+
+window.onscroll=()=>{
+
+    menu.classList.remove('fa-time');
+    navbar.classList.remove('active');
+
+    section.forEach(sec=>{
+
+      let top=window.scrollY;
+      let height=sec.offsetHeight;
+      let offset =sec.offsetTop-150;
+      let id=sec.getAttribute('id');
+
+      if(top=>offset && top < offset + height){
+        navLinks.forEach(links=>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+    });
+}
+
+document.querySelector('#search-icon').onclick=()=>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick=()=>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
+
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+   loop:true,
+  });
